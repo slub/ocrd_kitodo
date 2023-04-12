738cd2dce78202aef02b5fad05c81d9a6e267d1c
Markus Weigelt <Markus.Weigelt@slub-dresden.de>
2023-04-12 16:44:35 +0200

    Add workflow to create pull request for stable branch with release specific changes, modify update changelog script to autogenerate CHANGELOG.md without manual intervention

--- .github/scripts/update-changelog.sh
+++ .github/scripts/update-changelog.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
-version=$(date +'v%Y%m%d-stable')
-
 #token to request pr informations
 token=$1
-if [[ -z "$token" ]];then
-    echo "Could not detect token parameter version :("
+if [[ -z "$token" ]]; then
+    echo "Could not detect token parameter :("
     exit 1
 fi
 
+version=$2
+if [[ -z "$version" ]]; then
+    echo "Could not detect version parameter :("
+    exit 2
+fi
+
+previousVersionRef=$(git rev-list --tags --max-count=1)
+versionFromRef=$(git rev-list --children --reverse $previousVersionRef^.. | sed -n 1p | awk '{print $2}')
+versionToRef=$(git rev-list --max-count=1 HEAD)
 
 generate_changelog () {
     local path=$1
@@ -42,22 +49,40 @@ generate_changelog () {
     echo ""
 }
 
+
+generate_submodule_changelog () {
+    local module=$1
+    local title=$2
+    local fromCommit=$(git ls-tree $versionFromRef ./_modules/$module | awk '{print $3}')
+    local toCommit=$(git ls-tree $versionToRef ./_modules/$module | awk '{print $3}')
+
+    # deprecated because of moving OCR-D Monitor to separate repository
+    if [[ $module == "ocrd_monitor" && -z "$fromCommit" ]]; then
+        fromCommit=e9b41de42658f57361a0fe0ff56d673f225e4b4d
+    fi
+
+    generate_changelog "./_modules/$module" "$title" $fromCommit $toCommit slub/$module
+}
+
 (
 
-echo "# What's Changed"
+echo "# Changelog"
 echo ""
 echo "## [$version](https://github.com/slub/ocrd_kitodo/releases/tag/$version)"
 echo ""
 
-generate_changelog "./" "Integration of OCR-D and Kitodo" 79bda3b2f33181d7f4fcd13f02b482f912078eef 7cad5d37963a288697ec172350885c55dd7b1d21 slub/ocrd_kitodo
+generate_changelog "./" "Integration of OCR-D and Kitodo" $versionFromRef $versionToRef slub/ocrd_kitodo
 
-generate_changelog "./_modules/ocrd_controller" "OCR-D Controller" d3c0192d96bd4e75eadf27d19d9ba9b08297cf41 fc1a599506aeb165aacefd82238ff2fa75a2a8c6 slub/ocrd_controller
+generate_submodule_changelog ocrd_controller "OCR-D Controller"
 
-generate_changelog "./_modules/ocrd_manager" "OCR-D Manager" daed8299411dfb4f3476c5d8ea602ab9ac20c3a4 a42880baa54e6cbcc2b0b3bb877e4ae9b182a72c slub/ocrd_manager
+generate_submodule_changelog ocrd_manager "OCR-D Manager"
 
-generate_changelog "./_modules/ocrd_monitor" "OCR-D Monitor" e9b41de42658f57361a0fe0ff56d673f225e4b4d 48bdc8212eabec84ee5b1d387a35ce616bfdd945 slub/ocrd_monitor
+generate_submodule_changelog ocrd_monitor "OCR-D Monitor"
 
-generate_changelog "./_modules/kitodo-production-docker" "Kitodo.Production Docker" 5ad3a5d0b47c558e64fb5523c1a16a30b0028901 97fca72a4a37dfb21eacd331e1febc986113939a slub/kitodo-production-docker
+generate_submodule_changelog kitodo-production-docker "Kitodo.Production Docker"
+
+sed "/^[#] Changelog/d" CHANGELOG.md
 
 ) > CHANGELOG.md.tmp
+
 mv CHANGELOG.md.tmp CHANGELOG.md
